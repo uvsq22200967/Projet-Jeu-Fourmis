2e1c68abeadf221fd4d58fd5c1213b3623e8f2f3
Gladys TISBA <gladys.tisba@ens.uvsq.fr>
2023-03-17 13:21:43 +0100

    planprojet

--- planprojet.py
+++ planprojet.py
@@ -1,2 +1,22 @@
-hg
-ty
+#1 étape
+#Créer grille:(utilisation boucle for)
+#fonction 1: indiquer couleur case où est la fourmi 
+#fonction 2: si case noir pivotement 90° à gauche et si case blanche pivotement 90° à droite
+#Après pivotement faire une mise à jour
+#fonction 3: après passage fourmi , changer couleur de la casse en son inverse(si case blanc donc devient noir et inversement) 
+
+
+
+#2e étape
+#Présence de boutons:
+#Play:
+#Pause:
+#Next:
+#Accélérer vitesse:
+#Revenir en arrière:
+
+
+
+
+#3 étape:
+#Mettre une instance dans un fichier Texte, pour pouvoir revenir à ce moment 
