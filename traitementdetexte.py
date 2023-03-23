7aba07d164bcc5028deb7315e4a9f908a21f1034
Inès Mira <ines.mira@ens.uvsq.fr>
2023-03-23 18:15:54 +0100

    j'ai essayé.

--- traitementdetexte.py
+++ traitementdetexte.py
@@ -1,5 +1,12 @@
-hola
+WHITE= "white"
+BLACK= "black"
+RED= "red"
+BLUE= "lightblue"
 
-jul
+def get_flipped_color(color):
+    if color == "black":
+        return WHITE
+    if color == WHITE or color == BLUE:
+        return BLACK
 
-test
+
