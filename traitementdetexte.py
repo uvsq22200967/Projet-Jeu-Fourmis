b413eeb468bd229ffc67e90afa6917c7ef54ec43
Gladys TISBA <gladys.tisba@ens.uvsq.fr>
2023-03-24 13:37:12 +0100

    7

--- traitementdetexte.py
+++ traitementdetexte.py
@@ -1,12 +1,3 @@
-WHITE= "white"
-BLACK= "black"
-RED= "red"
-BLUE= "lightblue"
 
-def get_flipped_color(color):
-    if color == "black":
-        return WHITE
-    if color == WHITE or color == BLUE:
-        return BLACK
 
 
